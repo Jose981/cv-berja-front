d7bf0910ee03385adbdc1b6f5f16a73ff22b3480
Pepe MR <pepemartos98@gmail.com>
2026-04-15 20:01:33 +0200

    pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/cv-berja-front/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
